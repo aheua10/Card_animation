d240c534776021f241e0a2ac897074dcfbbf627f
Shai <aheua10@gmail.com>
2021-03-22 16:04:17 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import "./App.scss";
+import FirstCard from './Components/FirstCard/FirstCard';
+import SecondCard from "./Components/SecondCrad/SecondCard";
 
 export const App = () => {
   return (
+    <>
     <div className='headline-front-page'>
       <h1>
         Card Animations By S.E
       </h1>
     </div>
-    
+    <div>
+      <FirstCard />
+      </div>
+      <div>
+        <SecondCard /> 
+      </div>
+      <div className="third-section">
+        I Called It
+      </div>
+      </>
     )
 };
 export default App;
